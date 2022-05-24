21b4673d941e77279f3ebb3be085d8573d566647
Islam Khalitov <tu7m5rk73@aztu.local>
2022-05-24 19:37:57 +0400

    added

---
+++ PraktikaImport/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PraktikaImport.Models;
+using System.Collections.Generic;
+
+namespace PraktikaImport.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    Image = "A-Black-Racer-Snake.webp",
+                    Name = "Black Mamba",
+                    Info = "Lorem Lorem",
+                    Price = 10000
+                },
+                new Product
+                {
+                    Id = 2,
+                    Image = "Jaguar.jpg",
+                    Name = "Panther",
+                    Info = "Lorem Lorem",
+                    Price = 20000 
+                },
+                
+                new Product
+                {
+                    Id = 2,
+                    Image = "bald-eagle-closeup_4x3.webp",
+                    Name = "Eagle",
+                    Info = "Lorem Lorem",
+                    Price = 5000
+                }
+                
+            };
+            ViewBag.Products = products;
+            ViewData["Products"] = products;
+            return View(products);
+
+        }
+    }
+}
---
+++ PraktikaImport/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace PraktikaImport.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+        public string Info { get; set; }
+
+    }
+}
